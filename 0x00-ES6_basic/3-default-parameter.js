25d043da81348cf16fdd450f676a82fe1e282dd4
nomso <chinomsokoye@gmail.com>
2022-12-19 22:28:50 +0000

    Condense internal default parameters

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,5 +1,3 @@
-const getSumOfHoods = (initialNumber,
-		     expansion1989 = 89,
-		     expansion2019 = 19) => initialNumber + expansion1989 + expansion2019;
+const getSumOfHoods = (initialNumber, expansion1989 = 89, expansion2019 = 19) => initialNumber + expansion1989 + expansion2019;
 
 export default getSumOfHoods;
