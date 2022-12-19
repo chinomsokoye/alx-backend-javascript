5e291ca1009f50777ff6b4037f211f82e6cee55f
nomso <chinomsokoye@gmail.com>
2022-12-19 21:32:38 +0000

    Modify variables inside function taskBlock

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -7,8 +7,7 @@ export default function taskBlock(trueOrFalse) {
     const task = true;
     const task2 = false;
   }
-
-    /* eslint-disable no-use-vars */
+  /* eslint-disable no-use-vars */
 
   return [task, task2];
 }
