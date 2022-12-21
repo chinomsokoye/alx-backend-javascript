c0c4a297b00d25c6944618ae728488c6ae1fbb74
nomso <chinomsokoye@gmail.com>
2022-12-21 11:40:10 +0000

    Return a promise using prototype getFullResponseFromAPI(success), parameter is a boolean

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,7 +1,7 @@
 function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success) resolve({ status: 200, body: 'Success' });
-      reject(Error('The fake API is not working currently'));
+    reject(Error('The fake API is not working currently'));
   });
 }
 
