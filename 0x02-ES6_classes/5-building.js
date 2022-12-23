141eb280d17f18b89bbaa5dd8265958e15b0783c
nomso <chinomsokoye@gmail.com>
2022-12-23 03:46:24 +0000

    Implement Building class, ensure abstract and implement method evacuationWarningMessage

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,8 +1,8 @@
 export default class Building {
   constructor(sqft) {
     if (this.constructor !== Building
-	&& this.evacuationWarningMessage === undefined) {
-	throw Error('Class extending Building must override evacuationWarningMessage');
+      && this.evacuationWarningMessage === undefined) {
+      throw Error('Class extending Building must override evacuationWarningMessage');
     }
     this._sqft = sqft;
   }
