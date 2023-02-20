8aa4a3c72e5a9b1b4729bae0a9222a232b3a19fe
nomso <chinomsokoye@yahoo.com>
2023-02-20 15:29:28 +0000

    Read a file synchronously with Node JS

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+
+function countStudents(database) {
+  let students = [];
+  const StudentGroup = {};
+  const studentObject = [];
+
+  try {
+    students = fs.readFileSync(database, 'utf8');
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+
+  students = students.split('\n');
+  const header = students.shift().split(',');
+
+  students.forEach((element) => {
+    if (element) {
+      const studentInfo = element.split(',');
+
+      header.forEach((header, index) => {
+        studentObject[header] = studentInfo[index];
+	if (header === 'field') {
+	  if (StudentGroup[studentInfo[index]]) {
+            StudentGroup[studentInfo[index]].push(studentObject.firstname);
+	  } else {
+	    StudentGroup[studentInfo[index]] = [studentObject.firstname];
+	  }
+	}
+      });
+      studentObject.push(studentObject);
+    }
+  });
+  console.log(`Number of students: ${studentObject.length}`);
+
+  for (const info in StudentGroup) {
+    if (StudentGroup[info]) {
+      const listStudents = StudentGroup[info];
+      console.log(`Number of students in ${info}: ${listStudents.length}. List: ${listStudents.join(', ')}`);
+    }
+  }
+}
+
+module.exports = countStudents;
