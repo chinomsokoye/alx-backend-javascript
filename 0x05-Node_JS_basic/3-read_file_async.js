0676c44edc9fd9e2b13ca08b21918cfa88d8a699
nomso <chinomsokoye@yahoo.com>
2023-02-21 13:39:24 +0000

    Read a file asynchronously with Node JS

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -9,19 +9,25 @@ function countStudents(path) {
       }
       const response = [];
       let msg;
+
       const content = data.toString().split('\n');
+
       let students = content.filter((item) => item);
 
       students = students.map((item) => item.split(','));
+
       const NUMBER_OF_STUDENTS = students.length ? students.length - 1 : 0;
       msg = `Number of students: ${NUMBER_OF_STUDENTS}`;
       console.log(msg);
+
       response.push(msg);
+
       const fields = {};
       for (const i in students) {
         if (i !== 0) {
           if (!fields[students[i][3]]) fields[students[i][3]] = [];
-          fields[students[i][3]].push(students[i][0]);
+
+	  fields[students[i][3]].push(students[i][0]);
 	}
       }
 
@@ -31,7 +37,9 @@ function countStudents(path) {
         msg = `Number of students in ${key}: ${
           fields[key].length
         }. List: ${fields[key].join(', ')}`;
+
 	console.log(msg);
+
 	response.push(msg);
       }
       resolve(response);
