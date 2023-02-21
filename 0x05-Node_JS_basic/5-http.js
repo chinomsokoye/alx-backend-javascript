2aefc36759b0813c6fac897a7b7d522d435a5a73
nomso <chinomsokoye@yahoo.com>
2023-02-21 19:39:26 +0000

    Create a more complex HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,35 +1,31 @@
 const http = require('http');
+const students = require('./3-read_file_async');
 
-const args = process.argv.slice(2);
-const countStudents = require('./3-read_file_async');
-
-const DATABASE = args[0];
-
-const hostname = '127.0.0.1';
 const port = 1245;
 
-const app = http.createServer(async, (req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-
-  const { url } = req;
-  if (url === '/') {
-    res.write('Hello Holberton School!');
-  } else if (url === '/students') {
-    res.write('This is the list of our students\n');
-    try {
-      const students = await countStudents(DATABASE);
-      res.end(`${students.join('\n')}`);
-    } catch (error) {
-      res.end(error.message);
-    }
+const app = http.createServer((req, res) => {
+  switch (req.url) {
+    case '/':
+      res.writeHead(200);
+      res.end('Hello Holberton School!');
+      break;
+    case '/students':
+      res.writeHead(200);
+      res.write('This is the list of our students\n');
+      students(process.argv[2])
+	.then((data) => {
+	  res.end(data);
+	})
+        .catch((error) => {
+	  res.end(error.message);
+	});
+      break;
+    default:
+      res.writeHead(404);
+      res.end(JSON.stringify({ error: 'Resource not found' }));
   }
-  res.statusCode = 404;
-  res.end();
 });
 
-app.listen(port, hostname, () => {
-  // console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(port);
 
 module.exports = app;
