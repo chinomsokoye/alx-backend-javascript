20184e7da6abcdad6f3b93ab29d9c5c5762da4fe
nomso <chinomsokoye@yahoo.com>
2023-02-23 01:27:16 +0000

    Read a file asynchronously with Node JS

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-function countStudents (path) {
+function countStudents(path) {
   return new Promise((resolve, reject) => {
     fs.readFile(path, 'utf8', (err, data) => {
       if (err) {
